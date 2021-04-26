9132ef642893e4b213fb72d1bae2bd751230f820
diluo5261 <343917586@qq.com>
2021-04-26 20:46:09 +0800

    项目二练习_1

---
+++ project2/bean/Customer.java
@@ -0,0 +1,65 @@
+package day10.myproject.bean;
+
+/**
+ * Customer 为实体对象,用来封装客户信息
+ *
+ * */
+public class Customer {
+    //属性
+    String name;  //客户姓名
+    String gender; //性别
+    int age;  //年龄
+    String phone;  //点话号码
+    String email;  //邮箱
+
+    public Customer() {
+    }
+
+    public Customer(String name, String gender, int age, String phone, String email) {
+        this.name = name;
+        this.gender = gender;
+        this.age = age;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
